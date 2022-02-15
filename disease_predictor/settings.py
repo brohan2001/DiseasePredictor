08e6c62bde791b207da3dea5af7b0e8b01e8631c
sahilchanglani <sahilchanglani@gmail.com>
2022-02-15 14:57:52 -0800

    registered chats app

--- disease_predictor/settings.py
+++ disease_predictor/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main.apps.MainConfig',
+    'chats.apps.ChatsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
