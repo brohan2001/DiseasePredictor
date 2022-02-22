9852ffe8b6942538c03ef79e49a9a4c8d2e88bc7
sahilchanglani <sahilchanglani@gmail.com>
2022-02-22 14:51:59 -0800

    included urls

--- disease_predictor/urls.py
+++ disease_predictor/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include("main.urls")),
+    path('', include("main.urls")),
+     path("accounts/", include("accounts.urls")),
+     path("", include("chats.urls"))
 ]
 
