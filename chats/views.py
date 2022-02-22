a0fdb335b330cc9aa69440b33534dfd32d33cd92
sahilchanglani <sahilchanglani@gmail.com>
2022-02-22 14:51:29 -0800

    changes in chats app

--- chats/views.py
+++ chats/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from .models import Chat , Feedback
+from main.views import patient_ui, doctor_ui
+from main.models import patient , doctor
 
 # Create your views here.
+
+
+
+def post_feedback(request):
+    
+  if request.method == "POST":
+
+      feedback = request.POST.get('feedback', None)
+      if feedback != '':  
+        f = Feedback(sender=request.user, feedback=feedback)
+        f.save()        
+        print(feedback)   
+
+        try:
+           if (request.user.patient.is_patient == True) :
+              return HttpResponse("Feedback successfully sent.")
+        except:
+          pass
+        if (request.user.doctor.is_doctor == True) :
+           return HttpResponse("Feedback successfully sent.")
+
+      else :
+        return HttpResponse("Feedback field is empty   .")
+
+
+
+def get_feedback(request):
+    
+    if request.method == "GET":
+
+      obj = Feedback.objects.all()
+      
+      return redirect(request, 'consultation/chat_body.html',{"obj":obj})
+
+
