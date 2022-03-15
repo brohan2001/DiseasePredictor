6094fdd792d22dab17ec662ca7a492e492646c79
brohan2001 <rohan.balasubramanian@gmail.com>
2022-03-15 13:51:12 +0530

    changes in UI

--- disease_predictor/settings.py
+++ disease_predictor/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'predicare',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'rohan',
         'HOST': 'localhost'
     }
 }
